e28110424984e10a319d46fe23704717f11a2a44
michaelcohen716 <michaelcohen716@gmail.com>
2020-06-25 17:19:33 -0400

    clean out comment

--- contracts/interface/IKyberDAO.sol
+++ contracts/interface/IKyberDAO.sol
@@ -2,5 +2,4 @@ pragma solidity 0.5.15;
 
 contract IKyberDAO {
     function vote(uint256 campaignID, uint256 option) external;
-    // function claimReward(address staker, uint256 epoch) external;
 }
